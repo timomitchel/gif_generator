f1718d85cda8cebeeb78eaae2a9104b2304e0bcc
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-18 21:31:30 -0700

    search for gif

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -6,9 +6,10 @@ class Gif < ApplicationRecord
 
   belongs_to :category
 
-
-  url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=aWelUmFa12kmAdMsQPJrznBYgKXIosuM&limit=5"
-  resp = Net::HTTP.get_response(URI.parse(url))
-  buffer = resp.body
-  result = JSON.parse(buffer)
+  def self.grab_by_search
+    url = "http://api.giphy.com/v1/gifs/search?q=#{self.category}&api_key=aWelUmFa12kmAdMsQPJrznBYgKXIosuM&limit=5"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    buffer = resp.body
+    result = JSON.parse(buffer)
+  end
 end
