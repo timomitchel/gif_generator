74f4d9c746f47a61fad6b7774966ea66b6a2856d
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-19 20:34:04 -0700

    needs special admin login page

--- spec/features/admin_logs_in_spec.rb
+++ spec/features/admin_logs_in_spec.rb
@@ -5,22 +5,22 @@ describe "registered user logs in" do
     context "they click login link" do
       context "they enter username and password" do
         it "logs in a user" do
-          user = create(:user, role: 1)
+          admin = create(:user, role: 1)
+
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
           visit "/"
 
           click_on "Log In"
 
           expect(current_path).to eq(login_path)
-
-          fill_in "username", with: user.username
-          fill_in :password, with: user.password
+          fill_in 'sessions[username]', with: admin.username
+          fill_in 'sessions[password]', with: admin.password
 
           click_on "Log In"
 
-          expect(page).to have_content("Welcome, #{user.username}")
+          expect(page).to have_content("Welcome, #{admin.username}")
         end
       end
     end
   end
-
 end
